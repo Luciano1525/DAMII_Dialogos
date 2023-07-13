55dcc2153d0cc1c5c031bb5a615eb8b4aa47d6bb
Luciano Bacab <lucianobacab427@gmail.com>
2023-07-12 19:31:19 -0600

    Segundo Commit

--- app/src/main/java/com/example/congreso/MainActivity.java
+++ app/src/main/java/com/example/congreso/MainActivity.java
@@ -34,12 +34,67 @@ public class MainActivity extends AppCompatActivity {
                 res.setMessage("Seleccioone Registro");
                 View select = getLayoutInflater().inflate(R.layout.roless, null);
                 res.setView(select);
-                res.setCancelable(false);
+
+                btnExpo = (Button) select.findViewById(R.id.btnExpo);
+                btnExpo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder res1 = new AlertDialog.Builder(MainActivity.this);
+                        res1.setMessage("Registrar Conferencia");
+                        View select1 = getLayoutInflater().inflate(R.layout.custom_dialog_expositor, null);
+                        res1.setView(select1);
+                        res1.setCancelable(false);
+
+                        res1.setPositiveButton("Registrar Conferencia", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                        res1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        AlertDialog dialog1 = res1.create();
+                        dialog1.show();
+                    }
+                });
+
+                btnAsis = (Button) select.findViewById(R.id.btnAsis);
+                btnAsis.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder res1 = new AlertDialog.Builder(MainActivity.this);
+                        res1.setMessage("Consultar Conferencia");
+                        View select1 = getLayoutInflater().inflate(R.layout.custom_dialog_asistente, null);
+                        res1.setView(select1);
+                        res1.setCancelable(false);
+
+                        res1.setPositiveButton("Consultar Conferencia", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                        res1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        AlertDialog dialog1 = res1.create();
+                        dialog1.show();
+                    }
+                });
 
                 AlertDialog dialog = res.create();
                 dialog.show();
-
-
 
             }
         });
